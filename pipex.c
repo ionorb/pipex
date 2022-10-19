433b71e918baa266d8040e2a6696910085998999
Yoel RIDGWAY-LOPEZ <yridgway@e3r1p14.42.fr>
2022-10-19 17:13:52 +0200

    created functions to find the valid path of where the program is stored

--- pipex.c
+++ pipex.c
@@ -6,17 +6,52 @@
 /*   By: yridgway <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/13 17:23:44 by yridgway          #+#    #+#             */
-/*   Updated: 2022/10/18 18:41:56 by yridgway         ###   ########.fr       */
+/*   Updated: 2022/10/19 17:11:08 by yridgway         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
+char	**ft_get_paths(char **env)
+{
+	int	i;
+
+	i = 0;
+	while (env[i])
+	{
+		if (ft_strncmp(env[i], "PATH=", 5) == 0)
+		{
+			printf("%s\n", env[i] + 5);
+			return(ft_split(env[i] + 5, ":"));
+		}
+		i++;
+	}
+	return (NULL);
+}
+
+char	*get_valid_path(char **env)
+{
+	char	**paths;
+	int		i;
+
+	i = 0;
+	paths = ft_get_paths(env);
+	while (paths[i])
+	{
+		if (access(ft_strjoin(ft_strdup(paths[i]), "/push_swap_gui"), X_OK) == 0)
+			return (ft_strjoin(ft_strdup(paths[i]), "/push_swap_gui"));
+		i++;
+	}
+	free(paths);
+	return (NULL);
+}
+
 int	main(int ac, char **av, char **env)
 {
 	pid_t	pidz;
-	
+
 	(void)ac;
+	printf("valid: %s\n", get_valid_path(env));
 	pidz = fork();
 	if (pidz == -1)
 	{
--- pipex.h
+++ pipex.h
@@ -6,17 +6,18 @@
 /*   By: yridgway <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/13 17:23:51 by yridgway          #+#    #+#             */
-/*   Updated: 2022/10/13 17:48:57 by yridgway         ###   ########.fr       */
+/*   Updated: 2022/10/19 16:40:16 by yridgway         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 # define PIPEX_H
 
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <stdio.h>
-#include <errno.h>
-#include <unistd.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include <errno.h>
+# include <unistd.h>
+# include "libs/libft/libft.h"
 
 #endif
