d352e6be7de003de188c82adce2148fc42cb4e42
Yoel RIDGWAY-LOPEZ <yridgway@e3r1p14.42.fr>
2022-10-21 14:23:46 +0200

    redirected the stdin and stdout to pipefd thus creating a basic pipex program

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: yridgway <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/13 17:23:44 by yridgway          #+#    #+#             */
-/*   Updated: 2022/10/19 21:02:17 by yridgway         ###   ########.fr       */
+/*   Updated: 2022/10/21 14:19:54 by yridgway         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,27 +45,37 @@ char	*get_valid_path(char **env, char *prog)
 	return (NULL);
 }
 
+void	first_child(t_pipex *pipex, char **env)
+{
+	pipex->cmd1[0] = get_valid_path(env, pipex->cmd1[0]);
+	dup2(pipex->pipefd[1], 1);
+	execve(pipex->cmd1[0], pipex->cmd1, env);
+}
+
+void	second_child(t_pipex *pipex, char **env)
+{
+	pipex->cmd2[0] = get_valid_path(env, pipex->cmd2[0]);
+	dup2(pipex->pipefd[0], 0);
+	execve(pipex->cmd2[0], pipex->cmd2, env);
+}
+
 int	main(int ac, char **av, char **env)
 {
 	t_pipex	*pipex;
 
 	(void)ac;
 	pipex = malloc(sizeof (t_pipex));
-	pipe(pipex->pipefd);
+	if (!pipex || pipe(pipex->pipefd) == -1)
+		return (0);
+	printf("pipe1:%d, pipe2:%d\n", pipex->pipefd[0], pipex->pipefd[1]);
 	pipex->cmd1 = ft_split(av[1], " ");
 	pipex->cmd2 = ft_split(av[2], " ");
 	pipex->pid1 = fork();
 	if (pipex->pid1 == 0)
-	{
-		pipex->cmd1[0] = get_valid_path(env, pipex->cmd1[0]);
-		execve(pipex->cmd1[0], pipex->cmd1, env);
-	}
+		first_child(pipex, env);
 	pipex->pid2 = fork();
 	if (pipex->pid2 == 0)
-	{
-		pipex->cmd2[0] = get_valid_path(env, pipex->cmd2[0]);
-		execve(pipex->cmd2[0], pipex->cmd2, env);
-	}
+		second_child(pipex, env);
 	waitpid(pipex->pid1, NULL, 0);
 	waitpid(pipex->pid2, NULL, 0);
 	free_everything(pipex);
